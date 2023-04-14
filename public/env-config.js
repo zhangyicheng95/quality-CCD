92378e99a2e16246fc8fff07d9f5636b31636f7f
zhangyicheng95 <1239271010@qq.com>
2023-04-14 14:09:27 +0800

    feat: 0.8.16：放大镜

--- public/env-config.js
+++ public/env-config.js
@@ -1,6 +1,6 @@
 window.QUALITY_CCD_CONFIG = (function () {
     return {
         title: 'UBVision', // 系统默认名称 
-        type:'', // 哪个系统 vision的机器：vision ，光伏：gf
+        type:'gf', // 哪个系统 vision的机器：vision ，光伏：gf
     }
 })();
--- src/pages/home/components/Canvas/components/ImgCharts.tsx
+++ src/pages/home/components/Canvas/components/ImgCharts.tsx
@@ -14,7 +14,7 @@ interface Props {
 const ImgCharts: React.FC<Props> = (props: any) => {
     const { data = {}, id, } = props;
     let { dataValue, windowControl, setContentList } = data;
-    // dataValue = 'https://img1.baidu.com/it/u=4278632386,1202646523&fm=253&fmt=auto&app=138&f=JPEG?w=751&h=500';
+    dataValue = 'https://img95.699pic.com/xsj/0k/o5/ie.jpg%21/fw/700/watermark/url/L3hzai93YXRlcl9kZXRhaWwyLnBuZw/align/southeast';
     const { initialState } = useModel<any>('@@initialState');
     const { params } = initialState;
     const [fontSize, setFontSize] = useState({ width: '100%', height: 'auto' });
@@ -43,20 +43,25 @@ const ImgCharts: React.FC<Props> = (props: any) => {
         const mask: any = dom?.current?.querySelector('.mask');
         if (!eventDom || !mask) return;
         eventDom.onmousemove = function (event: any) {
-            //offsetX：鼠标坐标到元素的左侧的距离
-            //offsetWidth 除了外边距(margin)以外，所有的宽度(高度)之和
+            // offsetX：鼠标坐标到元素的左侧的距离
+            // offsetWidth 除了外边距(margin)以外，所有的宽度(高度)之和
             const { pageX = 0, pageY = 0, offsetX = 0, offsetY = 0 } = event;
+            let { clientWidth: bodyWidth, clientHeight: bodyHeight } = document.body;
+            let { clientWidth: boxWidth, clientHeight: boxHeight } = eventDom;
+
             let left = offsetX - mask?.offsetWidth / 2;
-            //offsetY：鼠标坐标到元素的顶部的距离
-            //offsetHeight:元素的像素高度 包含元素的垂直内边距和边框,水平滚动条的高度,且是一个整数
+            // offsetY：鼠标坐标到元素的顶部的距离
+            // offsetHeight:元素的像素高度 包含元素的垂直内边距和边框,水平滚动条的高度,且是一个整数
             let top = offsetY - mask?.offsetHeight / 2;
-            //  约束范围,保证光标在div范围内，都是以父盒子div为参考对象的
-            //超出图片左侧
+            // 约束范围,保证光标在div范围内，都是以父盒子div为参考对象的
+            // 超出图片左侧
             if (!left || (left <= 0)) left = 0;
             // 超出图片右侧
-            if (left >= mask?.offsetWidth) left = mask?.offsetWidth;
+            if ((left + mask?.offsetWidth) >= boxWidth) left = (boxWidth - mask?.offsetWidth);
+            // 超出图片上侧
             if (!top || (top <= 0)) top = 0;
-            if (top >= mask?.offsetHeight) top = mask?.offsetHeight;
+            // 超出图片下侧
+            if ((top + mask?.offsetHeight) >= boxHeight) top = (boxHeight - mask?.offsetHeight);
             // 修改元素的left|top属性值
             // 遮罩层
             mask.style['left'] = left + "px";
@@ -74,25 +79,56 @@ const ImgCharts: React.FC<Props> = (props: any) => {
             } else {
                 bigDom.style.display = 'block';
             }
-            let { clientWidth: bodyWidth, clientHeight: bodyHeight } = document.body;
-            // 放大镜的位置
-            const offset = 20;
-            if (pageX > bodyWidth / 2) {
-                bigDom.style['left'] = bodyWidth - pageX - offset + "px";
-            } else {
-                bigDom.style['left'] = pageX + offset + "px";
-            }
-            if (pageY > bodyHeight / 2) {
-                bigDom.style['top'] = bodyHeight - pageY - offset + "px";
-            } else {
-                bigDom.style['top'] = pageY + offset + "px";
-            }
+
             // 放大镜大小
-            bigDom.style['width'] = dom?.current?.clientWidth + "px";
-            bigDom.style['height'] = dom?.current?.clientHeight + "px";
-            // 与css中width：200%，height：200%相对应，建议以后放大倍数为2n
+            let bigWidth = boxWidth,
+                bigHeight = boxHeight,
+                scale = 1;
+            if (boxWidth > bodyWidth / 2) {
+                if (boxHeight > bodyHeight / 2) {
+                    if (((bodyWidth / 2) / boxWidth) >= ((bodyHeight / 2) / boxHeight)) {
+                        scale = (bodyWidth / 2) / boxWidth;
+                        bigWidth = bodyWidth / 2;
+                        bigHeight = boxHeight * scale;
+                    } else {
+                        scale = (bodyHeight / 2) / boxHeight;
+                        bigWidth = boxWidth * scale;
+                        bigHeight = bodyHeight / 2;
+                    }
+                } else {
+                    scale = (bodyWidth / 2) / boxWidth;
+                    bigWidth = bodyWidth / 2;
+                    bigHeight = boxHeight * scale;
+                }
+            } else if (boxHeight > bodyHeight / 2) {
+                scale = (bodyHeight / 2) / boxHeight;
+                bigWidth = boxWidth * scale;
+                bigHeight = bodyHeight / 2;
+            }
+            bigDom.style['width'] = bigWidth + "px";
+            bigDom.style['height'] = bigHeight + "px";
+            // 放大镜中的图片位置，与css中width：200%，height：200%相对应，建议以后放大倍数为2n
             imgDom.style['left'] = -4 * left + "px";
             imgDom.style['top'] = -4 * top + "px";
+            // 放大镜的位置
+            const offset = 20;
+            if ((offsetX > boxWidth / 2) && (offsetY < boxHeight / 2)) {
+                // 右上
+                bigDom.style['left'] = pageX - offset - bigWidth + "px";
+                bigDom.style['top'] = pageY + offset + "px";
+            } else if ((offsetX > boxWidth / 2) && (offsetY > boxHeight / 2)) {
+                // 右下
+                bigDom.style['left'] = pageX - offset - bigWidth + "px";
+                bigDom.style['top'] = pageY - offset - bigHeight + "px";
+            } else if ((offsetX < boxWidth / 2) && (offsetY < boxHeight / 2)) {
+                // 左上
+                bigDom.style['left'] = pageX + offset + "px";
+                bigDom.style['top'] = pageY + offset + "px";
+            } else if ((offsetX < boxWidth / 2) && (offsetY > boxHeight / 2)) {
+                // 左下
+                bigDom.style['left'] = pageX + offset + "px";
+                bigDom.style['top'] = pageY - offset - bigHeight + "px";
+            }
         }
         // 4.鼠标松开事件
         eventDom.onmouseleave = function (ev: any) {
