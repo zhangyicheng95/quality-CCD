331f4232a962d1d62675260f82fbd4535ad9bddf
zhangyicheng95 <1239271010@qq.com>
2023-04-19 16:31:20 +0800

    feat: 0.8.18：放大镜调整

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,10 +1,10 @@
 import { Route, Router, Switch, useHistory, } from "react-router";
 import Canvas from './components/Canvas';
 import { useReloadAfterStationary } from "@/utils/useReloadAfterStationary";
-import { useEffect } from "react";
 
 const Mark = (props: any) => {
   const history = useHistory();
+
   useReloadAfterStationary();
   return (
     <Router history={history}>
