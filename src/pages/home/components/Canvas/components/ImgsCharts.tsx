dbbcd5400cb891a4fe3bbaefed5a8c98978e9b32
zhangyicheng95 <1239271010@qq.com>
2023-05-15 14:51:58 +0800

    feat: 0.8.25：图片列表组件

--- src/pages/home/components/Canvas/components/ImgsCharts.tsx
+++ src/pages/home/components/Canvas/components/ImgsCharts.tsx
@@ -14,12 +14,19 @@ interface Props {
 const ImgsCharts: React.FC<Props> = (props: any) => {
     const { data = {}, id, } = props;
     let { dataValue, imgs_width: width = 150, imgs_height: height = 150, } = data;
+    if (process.env.NODE_ENV === 'development') {
+        dataValue = [
+            { value: 'https://img0.baidu.com/it/u=4285923189,2766387018&fm=253&fmt=auto&app=138&f=JPEG?w=281&h=499', name: 'data1' },
+            { value: 'https://img.zcool.cn/community/01a24d55efd0006ac7251df84f100f.jpg@3000w_1l_2o_100sh.jpg', name: 'data2' },
+            { value: 'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fimg9.doubanio.com%2Fview%2Fnote%2Fl%2Fpublic%2Fp61025315.jpg&refer=http%3A%2F%2Fimg9.doubanio.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1684313532&t=c46ef54f701c6b74cf0864410fcde75f', name: 'data3' },
+            { value: 'https://picnew9.photophoto.cn/20150712/huagongchangjianzhutupian-11762860_1.jpg', name: 'data4' },
+        ]
+    }
     const { initialState } = useModel<any>('@@initialState');
     const { params } = initialState;
     const dom = useRef<any>();
 
     const [imgList, setImgList] = useState([]);
-
 
     useEffect(() => {
         if (!_.isArray(dataValue)) {
@@ -28,29 +35,30 @@ const ImgsCharts: React.FC<Props> = (props: any) => {
             return;
         }
         if (_.isEmpty(dataValue)) return;
-        let result: any = [];
-        const loadFun = (item: any, index: number) => {
-            if (!item) return;
-            const { name, value } = item;
-            const img = document.createElement('img');
-            img.src = value;
-            img.title = name || 'img.png';
-            img.onload = (res: any) => {
-                const { width = 1, height = 1 } = img;
-                if (width > height) {
-                    result = result.concat({ ...item, imgHeight: '100%', imgWidth: 'auto' })
-                } else {
-                    result = result.concat({ ...item, imgHeight: 'auto', imgWidth: '100%' })
-                }
-                if (index + 1 === dataValue.length) {
-                    setImgList(result);
-                    return;
-                }
-                loadFun(dataValue[index + 1], index + 1);
-            };
-        };
-        loadFun(dataValue[0], 0);
-    }, [dataValue,]);
+        // let result: any = [];
+        // const loadFun = (item: any, index: number) => {
+        //     if (!item) return;
+        //     const { name, value } = item;
+        //     const img = document.createElement('img');
+        //     img.src = value;
+        //     img.title = name || 'img.png';
+        //     img.onload = (res: any) => {
+        //         const { width = 1, height = 1 } = img;
+        //         if (width > height) {
+        //             result = result.concat({ ...item, imgHeight: '100%', imgWidth: 'auto' })
+        //         } else {
+        //             result = result.concat({ ...item, imgHeight: 'auto', imgWidth: '100%' })
+        //         }
+
+        //         if (index + 1 === dataValue.length) {
+        //             setImgList(result);
+        //             return;
+        //         }
+        //         loadFun(dataValue[index + 1], index + 1);
+        //     };
+        // };
+        // loadFun(dataValue[0], 0);
+    }, []);
 
     return (
         <div
@@ -60,7 +68,7 @@ const ImgsCharts: React.FC<Props> = (props: any) => {
             ref={dom}
         >
             {
-                _.isArray(imgList) && (imgList || []).map((item: any, index: number) => {
+                _.isArray(dataValue) && (dataValue || []).map((item: any, index: number) => {
                     const { name, value, imgWidth, imgHeight } = item;
                     return <div
                         key={`echart-${id}-${index}`}
@@ -83,7 +91,7 @@ const ImgsCharts: React.FC<Props> = (props: any) => {
                                 <Image
                                     src={value}
                                     alt={name || 'logo'}
-                                    style={{ width: imgWidth, height: imgHeight, }}
+                                    style={{ width: 'auto', height: '100%', }}
                                 />
                             </div>
                             <TooltipDiv title={name} className="img-item-right-bottom">{name}</TooltipDiv>
