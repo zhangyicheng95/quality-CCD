979e1e32a5364b87be165fb00a292f76d48812d4
zhangyicheng95 <1239271010@qq.com>
2023-08-28 11:02:29 +0800

    feat: 0.8.50：表格组件调整

--- src/pages/home/components/Canvas/components/Table2Charts.tsx
+++ src/pages/home/components/Canvas/components/Table2Charts.tsx
@@ -324,29 +324,41 @@ const Table2Charts: React.FC<Props> = (props: any) => {
                                         if (_.isObject(val)) {
                                             // @ts-ignore
                                             const { value, color } = val;
-                                            return <TooltipDiv
-                                                className={`charts-body-td ${(_.isBoolean(interlacing) ? interlacing : true) ? 'charts-body-td-interlacing' : ''}`}
-                                                key={`echart-${id}-tr-td-${sIndex}-${value}`}
-                                                title={value?.length > 15 ? value : ''}
+                                            return <div className={`flex-box charts-body-td ${(_.isBoolean(interlacing) ? interlacing : true) ? 'charts-body-td-interlacing' : ''}`}>
+                                                <TooltipDiv
+                                                    className={`charts-body-td-title`}
+                                                    key={`echart-${id}-tr-td-${sIndex}-${value}`}
+                                                    title={value?.length > 15 ? value : ''}
+                                                    style={Object.assign(
+                                                        !!color ? { color } : {},
+                                                        des_bordered ? { borderWidth: '1px' } : {}
+                                                    )}
+                                                >
+                                                    {value}
+                                                </TooltipDiv>
+                                                {
+                                                    (!des_bordered || (index + 1) === dataValue?.length) ? null :
+                                                        <div className="charts-body-item-border" />
+                                                }
+                                            </div>
+                                        }
+                                        return <div className={`flex-box charts-body-td ${(_.isBoolean(interlacing) ? interlacing : true) ? 'charts-body-td-interlacing' : ''}`}>
+                                            <TooltipDiv
+                                                className={`charts-body-td`}
+                                                key={`echart-${id}-tr-td-${sIndex}-${val}`}
+                                                title={val?.length > 15 ? val : ''}
                                                 style={Object.assign(
                                                     !!color ? { color } : {},
                                                     des_bordered ? { borderWidth: '1px' } : {}
                                                 )}
                                             >
-                                                {value}
+                                                {val}
                                             </TooltipDiv>
-                                        }
-                                        return <TooltipDiv
-                                            className="charts-body-td"
-                                            key={`echart-${id}-tr-td-${sIndex}-${val}`}
-                                            title={val?.length > 15 ? val : ''}
-                                            style={Object.assign(
-                                                !!color ? { color } : {},
-                                                des_bordered ? { borderWidth: '1px' } : {}
-                                            )}
-                                        >
-                                            {val}
-                                        </TooltipDiv>
+                                            {
+                                                (!des_bordered || (index + 1) === dataValue?.length) ? null :
+                                                    <div className="charts-body-item-border" />
+                                            }
+                                        </div>
                                     })
                                 }
                             </div>
