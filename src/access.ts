443e6f7fe750522853af10b02ceed77b2345bf9d
zhangyicheng95 <1239271010@qq.com>
2024-04-07 11:21:13 +0800

    feat: 1.0.54: 窗口复制；操作项label显隐

--- src/access.ts
+++ src/access.ts
@@ -1,4 +1,4 @@
-import { getUserAuthList } from "./utils/utils";
+import { getUserAuthList } from './utils/utils';
 
 /**
  * @see https://umijs.org/zh-CN/plugins/plugin-access
@@ -7,12 +7,12 @@ export default function access(initialState: any) {
   const userAuthList = getUserAuthList();
 
   return {
-    canAdmin: true,//currentUser && currentUser.access === 'admin',
+    canAdmin: true, //currentUser && currentUser.access === 'admin',
     canMark: false,
     canHistory: userAuthList?.includes('history'),
     canControl: userAuthList?.includes('control'),
     canSetting: userAuthList?.includes('setting'),
-    canLog: userAuthList?.includes('log'),
-    canFileText: false //process.env.NODE_ENV === 'development'
+    canLog: true, //userAuthList?.includes('log'),
+    canFileText: false, //process.env.NODE_ENV === 'development'
   };
 }
