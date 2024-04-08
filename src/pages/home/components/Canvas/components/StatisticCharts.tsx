c2dee9d110d3b6aedf45a1bbe334bc9ed5f4c0a2
zhangyicheng95 <1239271010@qq.com>
2024-04-08 19:24:48 +0800

    feat: 1.0.55: 文本展示窗口兼容数据类型

--- src/pages/home/components/Canvas/components/StatisticCharts.tsx
+++ src/pages/home/components/Canvas/components/StatisticCharts.tsx
@@ -3,41 +3,51 @@ import styles from '../index.module.less';
 import * as _ from 'lodash';
 
 interface Props {
-    data: any,
-    id: any,
-    setMyChartVisible?: any,
-    onClick?: any,
+  data: any;
+  id: any;
+  setMyChartVisible?: any;
+  onClick?: any;
 }
 
 const StatisticCharts: React.FC<Props> = (props: any) => {
-    const { data = {}, id, } = props;
-    let { dataValue, fontSize, yName, fontColor, direction } = data;
-    if (process.env.NODE_ENV === 'development') {
-        dataValue = 10
-    }
-    return (
-        <div
-            id={`echart-${id}`}
-            className={`${styles.statisticCharts} flex-box`}
-            style={{
-                fontSize,
-                alignItems: direction
-            }}
-        >
-            <div className="statistic-title">{yName}</div>
-            <div className="statistic-value" style={Object.assign({
-                fontSize: Number(fontSize) + 10,
-            }, (!!fontColor && !!fontColor?.rgb) ? {
-                color: `rgba(${fontColor.rgb.r},${fontColor.rgb.g},${fontColor.rgb.b},${fontColor.rgb.a})`
-            } : {})}>{
-                    _.isString(dataValue) ?
-                        dataValue
-                        :
-                        JSON.stringify(dataValue)
-                }</div>
-        </div>
-    );
-
+  const { data = {}, id } = props;
+  let { dataValue, fontSize, yName, fontColor, direction } = data;
+  if (process.env.NODE_ENV === 'development') {
+    dataValue = 10;
+  }
+  return (
+    <div
+      id={`echart-${id}`}
+      className={`${styles.statisticCharts} flex-box`}
+      style={{
+        fontSize,
+        alignItems: direction,
+      }}
+    >
+      <div className="statistic-title">{yName}</div>
+      <div
+        className="statistic-value"
+        style={Object.assign(
+          {
+            fontSize: Number(fontSize) + 10,
+          },
+          !!fontColor && !!fontColor?.rgb
+            ? {
+                color: !!dataValue?.color
+                  ? dataValue?.color
+                  : `rgba(${fontColor.rgb.r},${fontColor.rgb.g},${fontColor.rgb.b},${fontColor.rgb.a})`,
+              }
+            : {},
+        )}
+      >
+        {_.isString(dataValue)
+          ? dataValue
+          : !!dataValue?.value
+          ? dataValue?.value
+          : JSON.stringify(dataValue)}
+      </div>
+    </div>
+  );
 };
 
 export default StatisticCharts;
